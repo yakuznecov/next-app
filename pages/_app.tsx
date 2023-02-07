9745d75d04edc904258519875def76a2bdd5465c
yakuznecov <yakuznecov@mail.ru>
2023-02-07 22:28:41 +0300

    add htag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>MyTop - наш лучший топ</title>
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
+
+export default App;
